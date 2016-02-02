2409ab4118a1be0bfca3bfd346687ad895e85ec7
Matt Condon <mattgcondon@gmail.com>
2016-02-02 20:17:12 +0700

    step 2

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 'use strict';
 
+let axios = require('axios');
 let express = require('express');
 let app = express();
 
+let WIT_AI_TOKEN = process.env.WIT_AI_TOKEN;
+
 app.get('/', function (req, res) {
 
-  console.log(req.query);
+  // Now that we have the query,
+  // lets ask wit to parse it and return some helpful information
+  axios({
+    url: 'https://api.wit.ai/message',
+    method: 'GET',
+    params: {
+      v: '20160202',
+      q: req.query.q,
+    },
+    headers: {
+      'Authorization': `Bearer ${WIT_AI_TOKEN}`,
+    },
+  })
+  .then(function(response) {
+    // for now, just print the response data
+    console.log(response.data);
+  })
+  .then(function() {
+    // tell Googiri we're done by completing the request
+    res.send('OK');
+  })
+  .catch(function(err) {
+    console.error(err);
+    res.send('Hello World!');
+  });
 
-  res.send('Hello World!');
 });
 
 let server = app.listen(8000, function () {
